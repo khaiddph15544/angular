884a1fd6457237a5584b1eed9931d96bcda957e1
khaidang1123 <akhai7230@gmail.com>
2022-03-23 18:18:32 +0700

    fix: theme header

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -8,22 +8,52 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
+    // this.slidefun(this.counter)
   }
-  monhoc: any = [
-    {
-      id: 1,
-      name: "Sản phẩm 1"
-    },
-    {
-      id: 2,
-      name: "Sản phẩm 2"
-    },
-    {
-      id: 3,
-      name: "Sản phẩm 3"
-    }
-  ]
+  myslide = document.querySelectorAll('.main')
 
+  counter = 1;
+  autoSlide = () => {
+    this.counter++;
+    this.slidefun(this.counter);
+  }
+  timer = setInterval(this.autoSlide, 6000);
+
+  dot = document.querySelectorAll('.dot')
+  plusSlides = (n: any): void => {
+    this.counter += n;
+    this.slidefun(this.counter);
+    this.resetTimer();
+  }
+  currentSlide = (n: any) => {
+    this.counter = n;
+    this.slidefun(this.counter);
+    this.resetTimer();
+  }
+  // this.slidefun(this.counter);
+  slidefun = (n: any): void => {
+    let i;
+    for (i = 0; i < this.myslide.length; i++) {
+      this.myslide[i].className = 'main';
+      console.log(this.myslide)
+    }
+    for (i = 0; i < this.dot.length; i++) {
+      this.dot[i].className = 'dot';
+    }
+    if (n > this.myslide.length) {
+      this.counter = 1;
+    }
+    if (n < 1) {
+      this.counter = this.myslide.length;
+    }
+    // this.myslide[this.counter - 1].className = 'active_main';
+    // this.dot[this.counter - 1].className += " dot_active";
+    // console.log(this.myslide)
+  }
+
+  resetTimer = () => {
+    clearInterval(this.timer);
+    this.timer = setInterval(this.autoSlide, 6000);
+  }
 }
